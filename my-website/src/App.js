788c1f02cd5a1dbc6d0d96acba41ddccd18eb8fa
ChasePham <chase9659@gmail.com>
2025-06-06 14:31:40 -0700

    added resume component code

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/Navbar.js';
 import Home from './components/Home.js';
 import About from './components/aboutMeSection/About.js'
 import Experience from './components/experienceSection/Experience.js'
+import Resume from './components/Resume.js';
 function App() {
   return (
     <div>
@@ -11,6 +12,7 @@ function App() {
       <Home></Home>
       <About></About>
       <Experience></Experience>
+      <Resume></Resume>
     </div>
   );
 }
---
+++ my-website/src/components/Resume.js
@@ -0,0 +1,12 @@
+function Resume() {
+
+    return(
+        <div>
+            <h2>Resume</h2>
+            <p>Fill In resume, figure out what tag is used for documents later</p>
+        </div>
+    )
+
+}
+
+export default Resume;
