6df79169e725310ab92bb7181b6c7c120db1f42d
ChasePham <chase9659@gmail.com>
2025-06-06 08:34:41 -0700

    added react component code for navbar

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
-
+import NavBar from './components/Navbar.js';
 function App() {
   return (
     <div className="App">
-      TODO:Fill In
+      <NavBar></NavBar>
     </div>
   );
 }
---
+++ my-website/src/components/Navbar.js
@@ -0,0 +1,16 @@
+
+function NavBar() {
+
+    return(
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Experience</li>
+            <li>Resume</li>
+        </ul>
+    </nav>
+    );
+}
+
+export default NavBar;
