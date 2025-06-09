3f7b0e1ca74f4abf1bba1cb7bf46b088048d36c8
ChasePham <chase9659@gmail.com>
2025-06-09 12:11:05 -0700

    added css for nav bar

--- my-website/src/components/Navbar.js
+++ my-website/src/components/Navbar.js
@@ -1,13 +1,14 @@
+import "./Navbar.css"
 
 function NavBar() {
 
     return(
-    <nav>
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Experience</li>
-            <li>Resume</li>
+    <nav class = "container-fluid">
+        <ul class = "d-flex list-unstyled justify-content-end">
+            <li class = "p-2">Home</li>
+            <li class = "p-2">About</li>
+            <li class = "p-2">Experience</li>
+            <li class = "p-2">Resume</li>
         </ul>
     </nav>
     );
