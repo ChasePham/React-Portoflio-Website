210c3bbedb37f464a8c2b7a7ee2a498356c86aa2
ChasePham <chase9659@gmail.com>
2025-06-09 12:55:53 -0700

    added css to home section

--- my-website/src/components/Home.js
+++ my-website/src/components/Home.js
@@ -2,20 +2,23 @@
 function Home() {
     //FIXME, IMG tag not working
     return(
-        <div>
-            <div>
-                <h1>Chase Pham</h1>
-                <h2>Software Engineer at Capital One</h2>
-                <ul>
-                    <li>Linkedin link</li>
-                    <li>Github Link</li>
-                    <li>Email Link</li>
-                    <li>Leetcode Link</li>
-                </ul>
+        <div class = "container">
+            <div class = "row">
+                <div class = "col">
+                    <h1 class = "text-center">Chase Pham</h1>
+                    <h2 class = "text-center">Software Engineer at Capital One</h2>
+                    <ul class = "d-flex list-unstyled justify-content-center">
+                        <li class = "p-2">Linkedin link</li>
+                        <li class = "p-2">Github Link</li>
+                        <li class = "p-2">Email Link</li>
+                        <li class = "p-2">Leetcode Link</li>
+                    </ul>
+                </div>
+                <div class = "col d-flex justify-content-center">
+                    <img src = "./components/gradimg.png" alt = "test"/>
+                </div>                
             </div>
-            <div>
-                <img src = "./components/gradimg.png" alt = "test"/>
-            </div>
+
         </div>
     )
 }
