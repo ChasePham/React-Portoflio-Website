9b9d6541e0f86c3d3e9bcfa02467fa182fc0c449
ChasePham <chase9659@gmail.com>
2025-08-14 20:40:39 -0400

    added carousel pics

--- my-website/src/components/aboutMeSection/About.js
+++ my-website/src/components/aboutMeSection/About.js
@@ -6,14 +6,15 @@ function About() {
         <div>
             <div class = "d-flex justify-content-center">
                 <div>
-                    <h2>About me</h2>
-                    <p>Fill in Bio</p>
+                    <h2 class = "text-center">About me</h2>
+                    <p class = "text-center">Currently, working at Capital One in DC as a Software Engineer focusing on full stack development. I recently graduated from the University of Texas at Austin where I studied Neuroscience and programming. On top of that, I've spent most of my learning tinkering on a variety of programming projects and networking with other like-minded software engineers, and I'm always looking for new ways to expand my skillset!
+</p>
                 </div>
 
             </div>
             <div  class = "d-flex justify-content-center">
-                <Hobby pics = {['test1','test2']} hobby = "My Life"></Hobby>
-                <Hobby pics = {['test1','test2']} hobby = "Music"></Hobby>
+                <Hobby pics = {['snowboard.jpg','hike.jpg','miami.jpg', 'boilerroom.jpg','beyond.jpg']} hobby = "My Life" id_name = "life"></Hobby>
+                {/* <Hobby pics = {['boilerroom.jpg','cfg.jpg','beyond.jpg',]} hobby = "Music" id_name= "music"></Hobby> */}
             </div>
 
         </div>
--- my-website/src/components/aboutMeSection/Hobby.js
+++ my-website/src/components/aboutMeSection/Hobby.js
@@ -1,15 +1,46 @@
 
 function Hobby(props) {
-    let list_items = []
-    for (let i = 0; i <props.pics.length; i++) {
-        list_items.push(<li>{props.pics[i]}</li>)
-    }
+        let list_items = props.pics.map((pic, i) => {
+            let src = "images/" + pic;
+            return (
+                <img
+                    key={i}
+                    className="img-fluid d-block w-100"
+                    src={src}
+                    alt="gradImgPng"
+                    width="60%"
+                    height="60%"
+                />
+            );
+        });
+    
+        // Step 2: Wrap each image in a carousel-item div
+        let carouselItems = list_items.map((img, i) => (
+            <div
+                className={`carousel-item${i === 0 ? " active" : ""}`}
+                key={i}
+            >
+                {img}
+            </div>
+        ));
+    
     return (
+        
         <div>
-            <h3 class = "p-2">{props.hobby}</h3>
-            <ul>
-                {list_items}
-            </ul>
+<div id={props.id_name} style= {{maxWidth: "500px"}}class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    {carouselItems}
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target={"#"+ props.id_name} data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target={"#"+ props.id_name} data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
         </div>
     )
 }
